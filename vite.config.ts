734a807dfa54071f2adfee2865f4f22179866ce1
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2026-06-10 20:25:34 -0400

    fix: stylex alias

--- vite.config.ts
+++ vite.config.ts
@@ -191,7 +191,7 @@ export default defineConfig({
 			? null
 			: stylex.vite({
 					aliases: {
-						'@/*': ['src/*'],
+						'@/*': [fileURLToPath(new URL('./src/*', import.meta.url))],
 					},
 					devMode: 'css-only',
 					devPersistToDisk: true,
