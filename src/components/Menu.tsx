7d7fc040de13e8e0afeee81994953a8760351b82
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2026-07-03 19:22:34 -0400

    feat: refine styling

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { ReactNode } from 'react'
 import { Link, useRouter } from 'waku'
 
 import { Box, Row } from '@/layouts/Box'
@@ -7,7 +8,9 @@ type Target = Parameters<typeof Link>[0]['to']
 
 export function Menu({
 	entries,
+	children,
 }: {
+	children?: ReactNode
 	entries: {
 		to: Target
 		title: string
@@ -15,12 +18,13 @@ export function Menu({
 }) {
 	const { path } = useRouter()
 	return (
-		<Row fontSize={4} gap={5}>
+		<Row align='baseline' fontSize={4} gap={5}>
 			{entries.map(({ to, title }) => (
 				<Box italic={path === to} as={Link} key={String(to)} to={to}>
 					{title}
 				</Box>
 			))}
+			{children}
 		</Row>
 	)
 }
--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -11,7 +11,6 @@ import { FaRss } from 'react-icons/fa6'
 import { Link } from 'waku'
 
 import { Menu } from '@/components/Menu'
-import { H1 } from '@/layouts/elements/Heading'
 import { Header } from '@/layouts/Header'
 import { borderWidth } from '@/layouts/tokens/borderWidth.stylex'
 import { colors } from '@/layouts/tokens/colors.stylex'
@@ -46,34 +45,33 @@ export default async function RootLayout({ children }: RootLayoutProps) {
 					justify='between'
 					style={styles.header}
 				>
-					<H1>
+					<Box fontSize={5} fontWeight='bold'>
 						<Link to='/'>{title}</Link>
-					</H1>
-					<Row align='center' gap={2}>
+					</Box>
+					<Row align='baseline' gap={2}>
 						<Menu
 							entries={[
 								{ to: '/', title: globalMessages.home },
 								{ to: '/blog', title: globalMessages.blog },
 							]}
-						/>
-						<Row
-							as='a'
-							href={basePath + 'rss.xml'}
-							title='RSS feed'
-							aria-label='RSS feed'
-							type='application/rss+xml'
-							w='toggleButton'
-							h='toggleButton'
-							align='center'
-							justify='center'
-							color='muted'
 						>
-							<FaRss aria-hidden='true' focusable='false' size={18} />
-						</Row>
+							<Row
+								as='a'
+								href={basePath + 'rss.xml'}
+								title='RSS feed'
+								aria-label='RSS feed'
+								type='application/rss+xml'
+								align='baseline'
+								justify='center'
+								color='muted'
+							>
+								<FaRss aria-hidden='true' focusable='false' size={16} />
+							</Row>
+						</Menu>
 					</Row>
 				</Row>
 			</Header>
-			<Col pt={8} pb={4} minW='readable' grow={1} as='main'>
+			<Col pt={8} pb={6} minW='readable' grow={1} as='main'>
 				{children}
 			</Col>
 			<Row gap={3} as='footer'>
